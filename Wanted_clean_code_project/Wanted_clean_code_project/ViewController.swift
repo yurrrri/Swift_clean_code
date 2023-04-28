2b13f1acd256b3bf9f2ffc8b285bc43aa7c731df
yurrrri <37764504+yurrrri@users.noreply.github.com>
2023-04-29 00:26:41 +0900

    🎉 원티드 프리온보딩 클린코드 프로젝트 시작!

---
+++ Wanted_clean_code_project/Wanted_clean_code_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wanted_clean_code_project
+//
+//  Created by 이유리 on 2023/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
